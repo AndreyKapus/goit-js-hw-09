add270608b00d3c15e1bb046e8fb77de18f35ccf
AndreyKapus <97341124+AndreyKapus@users.noreply.github.com>
2022-08-05 10:58:16 +0000

    Deploying to gh-pages from @ AndreyKapus/goit-js-hw-09@b6eb51d3b95299b672d096e7a0bd4a8483adac17 🚀

---
+++ 01-color-switcher.46e68428.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]");document.querySelector("body");console.log(t);t.addEventListener("click",(function(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}));
+//# sourceMappingURL=01-color-switcher.46e68428.js.map
