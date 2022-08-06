c448d07d45c108adf6e2cff5ef83bec6a5cea3ff
AndreyKapus <kapustnikov@ukr.net>
2022-08-06 14:04:59 +0300

    first task need to check

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,45 @@
 const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 const bgColor = document.querySelector('body');
 console.log(startButton);
+
+// ------------ Интервал-----------//
+const handleInterval = setInterval(changeBgColor, 2000);
+
+// /----------- Генератор цветов -------------/
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const handleClick = startButton.addEventListener('click', getRandomHexColor);
+
+// --------------- Кнопка старт ----------//
+const handleStartBtnClick = startButton.addEventListener(
+  'click',
+  changeBgColor
+);
+
+// function disableStartBtn() {
+//   if (handleStartBtnClick) {
+//     startButton.setAttribute('disabled', 'true');
+//     console.log('останавливаем!');
+//   }
+// }
+
+// ------------ Меняет цвет фона-------------//
+function changeBgColor() {
+  bgColor.style.backgroundColor = getRandomHexColor();
+  if (changeBgColor) {
+    startButton.disabled = true;
+  }
+}
+
+// -------------- Кнопка стоп ------------///
+const handleStopBtn = stopButton.addEventListener('click', stopInterval);
+
+// ------------- Очищает интервал -------------//
+function stopInterval() {
+  clearInterval(handleInterval);
+  if (stopInterval) {
+    startButton.disabled = false;
+  }
+}
