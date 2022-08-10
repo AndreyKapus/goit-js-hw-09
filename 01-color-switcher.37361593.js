b63691283fd96e811de18072bf4c863513a9e3b6
AndreyKapus <97341124+AndreyKapus@users.noreply.github.com>
2022-08-10 16:19:58 +0000

    Deploying to gh-pages from @ AndreyKapus/goit-js-hw-09@93bc980330c639ec77efe061104912bd45c67a6a 🚀

--- 01-color-switcher.37361593.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=setInterval(a,2e3);t.addEventListener("click",a);function a(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),a&&(t.disabled=!0)}e.addEventListener("click",(function e(){clearInterval(o),e&&(t.disabled=!1)}))}();
-//# sourceMappingURL=01-color-switcher.37361593.js.map
--- 01-color-switcher.c56b334b.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body"),n=setInterval(r,2e3);t.addEventListener("click",r);function r(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,r&&(t.disabled=!0)}e.addEventListener("click",(function e(){clearInterval(n),e&&(t.disabled=!1)}));
-//# sourceMappingURL=01-color-switcher.c56b334b.js.map
